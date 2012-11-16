6fc122218db37df50a79139df4e635b2058c2e37
toscho <info@toscho.de>
2012-11-16 02:49:25 +0100

    Use the first assigned tag for the permalink.
    See Christopher Davis’ solution:
    http://wordpress.stackexchange.com/questions/72703/multiple-tags-issue-in-permalinks/72712#72712

--- php/class.T5_Rewrite_Tag_Tag.php
+++ php/class.T5_Rewrite_Tag_Tag.php
@@ -22,14 +22,13 @@ class T5_Rewrite_Tag_Tag extends T5_Rewrite_Tag
 	 */
 	protected function get_link_replacement( $link, $post )
 	{
-		$tags = get_the_tags( $post->ID );
+		#$tags = get_the_tags( $post->ID );
+		$tag = $this->get_post_tag( $post->ID );
 
-		if ( ! $tags ) // insert a fallback value
+		if ( ! $tag ) // insert a fallback value
 			return str_replace( $this->tag, 'tag', $link );
 
-		$first = current( (array) $tags );
-
-		return str_replace( $this->tag, $first->slug, $link );
+		return str_replace( $this->tag, $tag, $link );
 	}
 
 	/**
@@ -51,5 +50,41 @@ class T5_Rewrite_Tag_Tag extends T5_Rewrite_Tag
 	protected function get_examples()
 	{
 		return 'cats, gifs';
+	}
+
+	/**
+	 * Make post tags sortable to assign the first tag to the permalink.
+	 *
+	 * (non-PHPdoc)
+	 * @see T5_Rewrite_Tag::init()
+	 */
+	public function init()
+	{
+		global $wp_taxonomies;
+
+		$wp_taxonomies['post_tag']->sort = true;
+
+		parent::init();
+	}
+
+	/**
+	 * Get the slug of first assigned tag.
+	 *
+	 * Solution by Christopher Davis.
+	 * @link http://wordpress.stackexchange.com/a/72712
+	 *
+	 * @see   init()
+	 * @param int $id          Post ID
+	 * @return boolean|string  FALSE if no tags are found, the slug otherwise.
+	 */
+	protected function get_post_tag( $id )
+	{
+		$args = array ( 'orderby' => 'term_order' );
+		$tags = wp_get_object_terms( $id, 'post_tag', $args );
+
+		if ( ! $tags )
+			return FALSE;
+
+		return current( (array) $tags )->slug;
 	}
 }
