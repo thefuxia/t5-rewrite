e41932cc32ba5d6a299f8ab9cd21f8e001e4b526
toscho <info@toscho.de>
2012-11-16 04:22:47 +0100

    Improve docs and formatting, add a new filter 't5_rewrite_subclasses'.

--- t5-rewrite.php
+++ t5-rewrite.php
@@ -87,21 +87,55 @@ class T5_Rewrite
 		add_action( 'in_admin_header', array ( $this, 'load_help' ) );
 	}
 
+	/**
+	 * Load classes for new rewrite tags.
+	 *
+	 * If you want to use your own class, hook into the action
+	 * 't5_rewrite_base_classes_loaded'.
+	 *
+	 * @wp-hook plugins_loaded
+	 * @return  void
+	 */
 	public function load_rewrite_tags()
 	{
-		$this->load_class( 'T5_Rewrite_Tag', FALSE );
-		$this->load_class( 'T5_Rewrite_Tag_Post_Format' );
-		$this->load_class( 'T5_Rewrite_Tag_Tag' );
-		$this->load_class( 'T5_Rewrite_Tag_Custom' );
+		$base_class  = 'T5_Rewrite_Tag';
+		$sub_classes = array ( 'Post_Format', 'Tag', 'Custom' );
+		$sub_classes = apply_filters(
+			't5_rewrite_tag_subclasses',
+			$sub_classes
+			);
+
+		$this->load_class( $base_class, FALSE );
+
+		if ( ! empty ( $sub_classes ) )
+		{
+			foreach ( $sub_classes as $sub_class )
+			{
+				$this->load_class( "{$base_class}_{$sub_class}" );
+			}
+		}
 
 		do_action( 't5_rewrite_base_classes_loaded' );
 	}
 
+	/**
+	 * Add a new help section to the permalinks page.
+	 *
+	 * @wp-hook in_admin_header
+	 * @return  void
+	 */
 	public function load_help()
 	{
 		$this->load_class( 'T5_Rewrite_Tags_Help' );
 	}
 
+	/**
+	 * Helper method to load classes from '/php/'.
+	 *
+	 * @param  string  $class    Class name
+	 * @param  boolean $instance Create a new instance automatically
+	 * @return object  $this
+	 */
 	protected function load_class( $class, $instance = TRUE )
 	{
 		$path = $this->plugin_path . "php/class.$class.php";
@@ -119,15 +153,11 @@ class T5_Rewrite
 	 *
 	 * @wp-hook wp_loaded
 	 * @param   string $domain
-	 * @since   2012.10.23
 	 * @return  void
 	 */
 	public function load_language( $domain )
 	{
-		load_plugin_textdomain(
-			$domain,
-			FALSE,
-			plugin_basename( dirname( __FILE__ ) ) . '/languages'
-		);
+		$path = plugin_basename( dirname( __FILE__ ) ) . '/languages';
+		load_plugin_textdomain( $domain, FALSE, $path );
 	}
 }
